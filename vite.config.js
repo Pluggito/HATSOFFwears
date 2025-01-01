47b36f20f7b970e23f3589c6c7e83c57b7243fd5
Pluggito <Pluggito@example.com>
2025-01-01 12:21:20 +0100

    new commits

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/HATSOFFwears",
   assetsInclude: ['**/*.PNG', '**/*.JPG', '**/*.jpg', '**/*.png'],
   optimizeDeps: {
     include: ['tailwindcss']
-  }
+  },
+  
 })
