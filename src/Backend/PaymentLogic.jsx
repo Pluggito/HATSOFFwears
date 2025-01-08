2dbbbbe49c7187465e2a68cfab47d18ea027fe6e
Etti Kehinde <ettiolayinka4@gmail.com>
2025-01-09 00:09:18 +0100

    Backend
    1. Payment with Flutterwave
    2. Save Order To FireBase
    3. Send email reciept to customer

---
+++ src/Backend/PaymentLogic.jsx
@@ -0,0 +1,43 @@
+import { useFlutterwave, closePaymentModal } from "flutterwave-react-v3";
+import { handlePlaceOrder } from "./OrderLogic";
+
+const paymentLogic = (userDetails, amount, cartItems, navigate) => {
+    const config = {
+        public_key: "FLWPUBK_TEST-d94776c57970d805d8371952a814636c-X",
+        tx_ref: Date.now(),
+        amount: amount,
+        currency: "NGN",
+        payment_options: "card,mobilemoney,ussd",
+        customer: {
+            email: userDetails.email,
+            phone_number: userDetails.phone,
+            name: `${userDetails.firstName} ${userDetails.lastName}`,
+        },
+        customizations: {
+            title: "HatsOff Store",
+            description: "Payment for items in cart",
+            logo: "https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg",
+        },
+    };
+
+    const handleFlutterPayment = useFlutterwave(config);
+
+    handleFlutterPayment({
+        callback: async (response) => {
+            if (response.status !== "completed") {
+                console.log("Payment Failed Try Again");
+            } else {
+                const trans_id = response.transaction_id;
+                await handlePlaceOrder(cartItems, userDetails, trans_id);
+                navigate('/order-summary');
+                console.log("Payment successful");
+            }
+            closePaymentModal();
+        },
+        onClose: () => {},
+    });
+};
+
+export const handlePayment = (userDetails, amount, cartItems, navigate) => {
+    paymentLogic(userDetails, amount, cartItems, navigate);
+};
